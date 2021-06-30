0c966b36b2410d6b6c75db52ab8084bbfafc0580
yennhisongnguyen <59899024+yennhisongnguyen@users.noreply.github.com>
2021-06-30 01:41:06 -0500

    Update chain.py

--- Assignment_1/source_code/chain.py
+++ Assignment_1/source_code/chain.py
@@ -3,7 +3,7 @@ from mpi4py import MPI
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
-#num = []
+#1 got error
 number = int(input('Enter a positive number, negative to stop: '))
 while True:
     if number < 0:
@@ -18,4 +18,16 @@ while True:
                 comm.isend(number, dest=rank + 1)
         print("Process %d got %d\n" % (rank, number))
 
+#2 new
+user_number = 0
+while user_number >= 0:
+    if rank == 0:
+        print("Enter a number?")
+        user_number = int(input())
+        comm.send(user_number, dest=rank + 1)
+    else:
+        comm.recv(user_number, source=rank - 1)
+        if rank < size - 1:
+            comm.send(user_number, dest=rank + 1)
+    print("Process %d got %d\n" % (rank, user_number))
 
