d9b0e3eb66ba25f75eb3f922378a90c3171e6ba4
kjf822 <kjf822@my.utsa.edu>
2021-07-03 19:18:28 -0500

    Final working version

--- Assignment_1/source_code/chain.py
+++ Assignment_1/source_code/chain.py
@@ -1,1 +1,32 @@
+from mpi4py import MPI
 
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+num = 0
+while rank < size:
+    if rank == 0:
+        if num != 0:
+            req = comm.irecv(source=size-1)
+            num = req.wait()
+        num = int(input("Enter a positive number, negative will stop: "))
+        if num < 0:
+            for r in range(1, size):
+                comm.isend(0, dest=r, tag=11)
+            comm.Abort()
+        comm.isend(num, dest=rank+1)
+
+    if rank > 0 and rank < size-1:
+        req = comm.irecv(source=rank-1)
+        num = req.wait()
+        if num != 0:
+            print("Process %d got %d\n" % (rank, num))
+        comm.isend(num, dest=rank+1)
+
+    if rank == size-1:
+        req = comm.irecv(source=rank-1)
+        num = req.wait()
+        if num != 0:
+            print("Final Process got %d\n" % num)
+        comm.isend(num, dest=0)
--- Assignment_1/source_code/search.py
+++ Assignment_1/source_code/search.py
@@ -1,1 +1,53 @@
+from mpi4py import MPI
 
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+
+f = open("data.txt", "r").read().split("\n")
+req = comm.irecv(source=MPI.ANY_SOURCE, tag=11)
+if rank == 0:
+    for i in f[0:10000]:
+        if(req.Test() == True):
+            index = f.index(i)
+            print("Master process has received message at index %d" % (index))
+            break
+        if i == "11":
+            for r in range(1, size):
+                comm.isend(0, dest=r, tag=11)
+            index = f.index(i)
+            print("I am master process and I found 11 at index %d" % (index))
+if rank == 1:
+    for i in f[10000:20000]:
+        if(req.Test() == True):
+            index = f.index(i)
+            print("Process 1 has received message at index %d" % (index))
+            break
+        if i == "11":
+            for r in range(1, size):
+                comm.isend(0, dest=r, tag=11)
+            index = f.index(i)
+            print("I am process %d and I found 11 at index %d" % (rank, index))
+if rank == 2:
+    for i in f[20000:30000]:
+        if(req.Test() == True):
+            index = f.index(i)
+            print("Process 2 has received message at index %d" % (index))
+            break
+        if i == "11":
+            for r in range(1, size):
+                comm.isend(0, dest=r, tag=11)
+            index = f.index(i)
+            print("I am process %d and I found 11 at index %d" % (rank, index))
+if rank == 3:
+    for i in f[30000:40000]:
+        if(req.Test() == True):
+            index = f.index(i)
+            print("Process 3 has received message at index %d" % (index))
+            break
+        if i == "11":
+            for r in range(1, size):
+                comm.isend(0, dest=r, tag=11)
+            index = f.index(i)
+            print("I am process %d and I found 11 at index %d" % (rank, index))
