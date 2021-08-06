3ef387aa50f6e987fbf84948c350debb98360afa
jassygirl00 <jasminebeale0521@gmail.com>
2021-08-06 17:06:16 -0500

    uploaded working program

--- Assignment_3/assign3.py
+++ Assignment_3/assign3.py
@@ -3,28 +3,21 @@ from pyspark.mllib.clustering import KMeans
 #import KMeans from pyspark
 from pyspark import SparkContext 
 import numpy as np
+import re
 
 sc = SparkContext(appName="MySparkProg")
 sc.setLogLevel("ERROR")
-rdd = sc.textFile("hdfs://10.56.2.193:54310/hw2-input/")
+data = sc.textFile("hdfs://10.56.2.193:54310/user/cc/hw2-input/")
 # use csv reader to split each line of file into a list of elements.
 # this will automatically split the csv data correctly.
-rdd = rdd.mapPartitions(lambda x: reader(x))
+rdd = data.mapPartitions(lambda x: reader(x))
+rdd = rdd.filter(lambda x: (re.match(r'07\/[0-9]{2}\/[0-9]{4}', x[5])))
+crimes = rdd.map(lambda x: (x[7],1)).reduceByKey(lambda x,y: x+y).sortBy(lambda x: x[1], False).take(3)
 
-# using filter with column condition (crimes in month of july)
-for i in rdd.select("OFNS_DESC"):
-    rdd = rdd.filter(rdd.RPT_DT == "july")
-
-# perform sum all the int values for each unique keys
-
-rdd = rdd.reduceByKey(lambda x, y: x + y)
-
-#function grab top 3
-rdd = rdd.map(lambda x: (x[1], x[0])).sortByKey(False).take(3)
-
-#print out
-print("Top 3 crime types that were reported in the month of July are:")
-
-#How many crimes of type DANGEROUS WEAPONS were reported in the month of July ?
-rdd_count = rdd.filter(rdd.RPT_DT == "july" and rdd.OFNS_DESC == "DANGEROUS WEAPONS")
-rdd_count.count()
+print("The top 3 types of crime committed in July are as follows:")
+print(crimes)
+# How many crimes of type DANGEROUS WEAPONS were reported in the month of July?
+rdd = data.mapPartitions(lambda x: reader(x))
+rdd = rdd.filter(lambda x: ("DANGEROUS WEAPONS" in x[7]))
+rdd = rdd.filter(lambda x: (re.match(r'07\/[0-9]{2}\/[0-9]{4}', x[5])))
+print("There were %s crimes of type \'DANGEROUS WEAPONS\' committed in the month of July" % (rdd.count()))
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Link to assignment 1 report document: https://docs.google.com/document/d/13_wcHqrLEad9ibLdjMSF8XZ6C6An9C7ZOngwSDd2kQA/edit?usp=sharing
 
 Link to assignment 2 report document: https://docs.google.com/document/d/1e832OIbPPgvLTNO8DscfU3dxjQW3z4ihX0D0cOap3L0/edit?usp=sharing
+
+Link to assignment 3 report doc (accidently overwrote previous one): https://docs.google.com/document/d/1e832OIbPPgvLTNO8DscfU3dxjQW3z4ihX0D0cOap3L0/edit?usp=sharing
